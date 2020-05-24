bf561a712f9426644e628b70225881ac6dc1d43b
OmarHadhoud <OmarHadhoud@ymail.com>
2020-05-24 19:17:48 +0200

    Implemented semaphores

--- sem.c
+++ sem.c
@@ -2,7 +2,8 @@
 
 //To allocate and free memory we need stdlib.
 #include <stdlib.h>
-
+//To send signal to process acquiring the semaphore to continue.
+#include <signal.h>
 /*
  * Semaphores functions.
  */
@@ -11,23 +12,70 @@
  *  Initializes the semaphore
  */
 
-void sem_initialize(struct sem *s, int N)
+void sem_initialize(struct Sem *s)
 {
-    int pid_size = sizeof(pid_t); //The maximum size of the string of process id.
-    s->locked = fale;
-    s->sem_holder =  malloc(pid_size);
+    int pid_size = sizeof(pid_t); //The maximum size of process id.
+    s->locked = 0;
+    s->sem_holder = malloc(pid_size);
     //If we ran out of memory, return.
     if (s->sem_holder == NULL)
     {
         s = NULL;
         return;
     }
-    //Queue linked list s->waiting_queue = malloc(N*pid_size); //Every cell in the array is a process id waiting for queue.
+    s->waiting_queue = create_queue();
     //If we ran out of memory, free the sem_holder and return.
-    if(s->waiting_queue == NULL)
+    if (s->waiting_queue == NULL)
     {
-        free s->sem_holder;
+        free(s->sem_holder);
         s = NULL;
         return;
     }
+}
+
+/*
+ *  Deletes the semaphore and frees any memory dynamically allocated.
+ */
+void sem_delete(struct Sem *s)
+{
+    free(s->sem_holder);
+    delete_queue(s->waiting_queue);
+}
+
+/*
+ *  Acquires the semaphor to the passed process.
+ *  It sends a signal to wake the process if it can acquire the semaphore,
+ *  if it can't, it will add it to the waiting queue.
+ */
+void acquire_sem(struct Sem *s, int pid)
+{
+    if (s->locked == 0)
+    {
+        //Acquire the sem, wake the process.
+        s->locked = 1;
+        s->sem_holder = pid;
+        kill(pid, SIGCONT);
+    }
+    else //Add the process to the waiting queue.
+        enqueue(s->waiting_queue, pid);
+}
+
+/*
+ *  Releases the semaphor passed.
+ *  Wakes up the next process in waiting queue of this semaphore, if there was any.
+ */
+void release_sem(struct Sem *s, int pid)
+{
+    if (s->sem_holder != pid) //Make sure the one releasing sem is the holder.
+        return;
+
+    s->locked = 0;
+    s->sem_holder = NULL;
+    //If no process is in waiting queue, return.
+    if (s->waiting_queue->front == NULL)
+        return;
+    //Get the next process id to acquire the semaphore.
+    int next_pid = s->waiting_queue->front;
+    dequeue(s->waiting_queue);
+    acquire_sem(s, next_pid); //Give the semaphore to the process waiting.
 }
--- sem.h
+++ sem.h
@@ -1,16 +1,19 @@
 #ifndef _SEM_H_
 #define _SEM_H_
 
+//Including the queue header needed for waiting queue.
+#include "queue.h"
+
 //Definition of semaphore
 
 /*
  * Semaphore structure.  
  */
-struct sem {
+struct Sem {
     _Bool locked; //If locked, no process can acquire this semaphore until it is released.
     char *sem_holder; //The id of the process that holds the semaphore. Should equal NULL if sem is not locked.
-    //Queue **waiting_queue; //The queue of ids of the processes waiting on this semaphore to be released. It is a queue [FIFO].
-}
+    struct Queue *waiting_queue; //The queue of ids of the processes waiting on this semaphore to be released. It is a queue [FIFO].
+};
 
 /*
  * Semaphore functions.
@@ -18,15 +21,19 @@ struct sem {
  * sem_initialize: Initializes the semaphore for N clients.
  * sem_delete: Deletes the semaphore with the arrays created for it.
  *  
- * acquire_sem: The passed process will try to acquire the semaphore, and will get blocked if semaphore is already locked.
- * release_sem: The passed process will try to release the semaphore, this can only happen if it's the one holding the semaphore.
+ * acquire_sem: The passed process will try to acquire the semaphore,
+ * and will get blocked if semaphore is already locked.
+ * 
+ * release_sem: The passed process will try to release the semaphore,
+ * this can only happen if it's the one holding the semaphore. It will
+ * give the semaphore to the next process waiting on the sem queue.
  */
 
-void sem_initialize(struct sem *s);
-void sem_delete(struct sem *s);
+void sem_initialize(struct Sem *s);
+void sem_delete(struct Sem *s);
 
-void acquire_sem(struct sem *s, char *pid);
-void release_sem(struct sem *s, char *pid);
+void acquire_sem(struct Sem *s, int pid);
+void release_sem(struct Sem *s, int pid);
 
 
-#endife /* _SEM_H_ */
+#endif /* _SEM_H_ */
