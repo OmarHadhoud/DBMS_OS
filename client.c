321b2c326e101fb4da3da1fe1c5560d3a47424e7
OmarHadhoud <OmarHadhoud@ymail.com>
2020-05-29 10:33:43 +0200

    Added basic functions for queries

--- client.c
+++ client.c
@@ -1,6 +1,7 @@
 #include "client.h"
 
-
+#include <stdio.h>
+#include <string.h>
 
 /*
  * Client functions.
@@ -13,16 +14,16 @@ void client_main()
 {
     printf("I'm the client, my pid is : %d\n", getpid());
     //Get the db shared memory as read-only for queries
-    client_shm_record = (struct record*) shmat(sys_info.records_shmid,NULL,SHM_RDONLY);
-    char *msg="help me please";
-    char *msg2="help me more";
-    logger_shared_memory = (struct LoggerSharedMemory*) shmat(sys_info.logger_shmid,NULL,0);
+    client_shm_records = (struct ManagerSharedMemory *)shmat(sys_info.records_shmid, NULL, SHM_RDONLY);
+    char *msg = "help me please";
+    char *msg2 = "help me more";
+    logger_shared_memory = (struct LoggerSharedMemory *)shmat(sys_info.logger_shmid, NULL, 0);
     Produce(msg);
     Produce(msg2);
     acquire_query_logger_sem();
     release_query_logger_sem();
     //Detach shared memory segments
-    shmdt(client_shm_record);
+    shmdt(client_shm_records);
     shmdt(logger_shared_memory);
 }
 
@@ -36,16 +37,16 @@ void client_add_record(char name[20], int salary, int key)
     buff.message_record.key = key;
     buff.message_record.salary = salary;
     buff.pid = getpid();
-    for(int i = 0; i<=20; i++)
+    for (int i = 0; i <= 20; i++)
     {
-        if(name[i] >= 'a' && name[i] <= 'z')
+        if (name[i] >= 'a' && name[i] <= 'z')
             buff.message_record.name[i] = name[i];
     }
     int send_val = msgsnd(sys_info.dbmanager_msgqid, &buff, sizeof(buff.message_record), 0);
     if (send_val == -1)
         perror("Error in send");
-        
-    kill(sys_info.db_manager_pid,SIGCONT);
+
+    kill(sys_info.db_manager_pid, SIGCONT);
     raise(SIGSTOP);
 
     int rec_val = msgrcv(sys_info.dbmanager_msgqid, &buff, sizeof(buff.message_record), getpid(), 0);
@@ -53,7 +54,6 @@ void client_add_record(char name[20], int salary, int key)
         perror("Error in recieve");
 
     key = buff.message_record.key;
-
 }
 
 /*
@@ -70,7 +70,7 @@ void client_modify(int key, int value)
     if (send_val == -1)
         perror("Error in send");
 
-    kill(sys_info.db_manager_pid,SIGCONT);
+    kill(sys_info.db_manager_pid, SIGCONT);
     raise(SIGSTOP);
 }
 
@@ -87,8 +87,8 @@ void client_acquire(int key)
     if (send_val == -1)
         perror("Error in send");
 
-    kill(sys_info.db_manager_pid,SIGCONT);
-    raise(SIGSTOP);    
+    kill(sys_info.db_manager_pid, SIGCONT);
+    raise(SIGSTOP);
 }
 
 /*
@@ -96,7 +96,7 @@ void client_acquire(int key)
  */
 void client_release(int key)
 {
-   struct message buff;
+    struct message buff;
     buff.mtype = sys_info.db_manager_pid;
     buff.message_record.key = key;
     buff.pid = getpid();
@@ -104,6 +104,136 @@ void client_release(int key)
     if (send_val == -1)
         perror("Error in send");
 
-    kill(sys_info.db_manager_pid,SIGCONT); 
+    kill(sys_info.db_manager_pid, SIGCONT);
     raise(SIGSTOP);
+}
+
+/*
+ * Selects all records from the db.
+ */
+void select_all()
+{
+    acquire_query_logger_sem();
+    //Critical section, write to file the records directly
+    //Open file to append in
+    FILE *F = fopen(QUERY_LOGGER_FILE_NAME, "a");
+    if (F == NULL)
+        perror("Couldn't create file");
+    struct record *rec = client_shm_records;
+    for (int i = 0; i < 1000; i++)
+    {
+        printf("%d\t%c\t%d\n", rec->key, rec->name, rec->salary);
+        fflush(F);
+    }
+    //Close the file after writing and release the lock
+    fclose(F);
+    release_query_logger_sem();
+}
+
+/*
+ * Selects all records from the db starting with name or equal to name depending on exact bool.
+ */
+void select_name(char *name, int exact)
+{
+    acquire_query_logger_sem();
+    //Critical section, write to file the records directly
+    //Open file to append in
+    FILE *F = fopen(QUERY_LOGGER_FILE_NAME, "a");
+    if (F == NULL)
+        perror("Couldn't create file");
+    struct record *rec = client_shm_records;
+    for (int i = 0; i < 1000; i++)
+    {
+        if (!check_name(rec->name,name,exact))
+            continue;
+        printf("%d\t%c\t%d\n", rec->key, rec->name, rec->salary);
+        fflush(F);
+    }
+    //Close the file after writing and release the lock
+    fclose(F);
+    release_query_logger_sem();
+}
+
+/*
+ * Selects all records from the db depending on the salary and the mode.
+ */
+void select_salary(int salary, int mode)
+{
+    acquire_query_logger_sem();
+    //Critical section, write to file the records directly
+    //Open file to append in
+    FILE *F = fopen(QUERY_LOGGER_FILE_NAME, "a");
+    if (F == NULL)
+        perror("Couldn't create file");
+    struct record *rec = client_shm_records;
+    for (int i = 0; i < 1000; i++)
+    {
+        if (!check_salary(rec->salary,salary,mode))
+            continue;
+
+        printf("%d\t%c\t%d\n", rec->key, rec->name, rec->salary);
+        fflush(F);
+    }
+    //Close the file after writing and release the lock
+    fclose(F);
+    release_query_logger_sem();
+}
+
+/*
+ * Selects all records from the db with hybrid query depending on both salary and name.
+ */
+void select_hybrid(char *name, int salary, int mode, int exact)
+{
+    acquire_query_logger_sem();
+    //Critical section, write to file the records directly
+    //Open file to append in
+    FILE *F = fopen(QUERY_LOGGER_FILE_NAME, "a");
+    if (F == NULL)
+        perror("Couldn't create file");
+    struct record *rec = client_shm_records;
+    for (int i = 0; i < 1000; i++)
+    {
+        if (check_name(rec->name, name, exact) == 0 || check_salary(rec->salary, salary, mode) == 0)
+            continue;
+
+        printf("%d\t%c\t%d\n", rec->key, rec->name, rec->salary);
+        fflush(F);
+    }
+    //Close the file after writing and release the lock
+    fclose(F);
+    release_query_logger_sem();
+}
+
+/*
+ * Checks if the condition on the name is true or not.
+ */
+_Bool check_name(char *real_name, char *name, int exact)
+{
+
+    if (exact == 1 && strcmp(name, real_name) != 0)
+        return 0;
+    if (exact == 0)
+    {
+        if (strlen(name) > strlen(real_name) || memcmp(name, real_name, strlen(name)) != 0)
+            return 0;
+    }
+    return 1;
+}
+
+/*
+ * Checks if the condition on the salary is true or not.
+ */
+_Bool check_salary(int real_salary, int salary, int mode)
+{
+    if (mode == 0 && !(real_salary == salary))
+        return 0;
+    if (mode == 1 && !(real_salary > salary))
+        return 0;
+    if (mode == 2 && !(real_salary < salary))
+        return 0;
+    if (mode == 3 && !(real_salary >= salary))
+        return 0;
+    if (mode == 4 && !(real_salary <= salary))
+        return 0;
+    return 1;
 }
--- client.h
+++ client.h
@@ -12,10 +12,9 @@
  * Process variables and structs.  
  */
 
-struct record* client_shm_record;
+struct record* client_shm_records;
 //The name of the file to write queries in
 #define QUERY_LOGGER_FILE_NAME "queries.txt"
-
 
 
 /*
@@ -26,6 +25,12 @@ struct record* client_shm_record;
  * client_modify: Tell the manager, that there is a record with the certain key, I want to add/sub this value to this salary.
  * client_acquire: Tell the manager I want to lock a certain record.
  * client_release: Send a key to the manager in a message, release it’s key.
+ * select_all: Selects all records from the DB.
+ * select_name: Select with name starting with or exact name.
+ * select_salary: Select with name starting with salary ==,>,<,>=,<=.
+ * select_hybrid: Select with name starting with or exact name and with salary condition.
+ * check_name: Checks if the condition on the name is true or not.
+ * check_salary: Checks if the condition on the salary is true or not.
  */
 
 void client_main();
@@ -33,6 +38,12 @@ void client_add_record(char name[20], int salary, int key);
 void client_modify(int key, int value);
 void client_acquire(int key);
 void client_release(int key);
+void select_all();
+void select_name(char *name, int exact);
+void select_salary(int salary, int mode);
+void select_hybrid(char *name, int salary, int mode, int exact);
 
+_Bool check_name(char* real_name, char *name, int exact);
+_Bool check_salary(int real_salary, int salary, int mode);
 
 #endif /* _CLIENT_H_ */
